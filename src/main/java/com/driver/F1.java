3109c29d77018cc6bd3b0eff4ee55fc62dfe6a19
jainpranshu21 <pranshujain80@gmail.com>
2023-04-19 15:54:45 +0530

    Assignment Done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,6 +4,8 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+      this.setName(name);
+      this.setManual(isManual);
     }
 
     public void accelerate(int rate){
@@ -17,12 +19,26 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-
+        newSpeed=getCurrentSpeed()+rate;
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            super.stop();
+            setCurrentGear(1);
         }
         //for all other cases, change the gear accordingly
-
+        else if (newSpeed>0) {
+            setCurrentGear(1);
+        } else if (newSpeed>50) {
+            setCurrentGear(2);
+        } else if (newSpeed>100) {
+            setCurrentGear(3);
+        } else if (newSpeed>150) {
+            setCurrentGear(4);
+        } else if (newSpeed>200) {
+            setCurrentGear(5);
+        }
+        else if (newSpeed>250)
+            setCurrentGear(6);
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
